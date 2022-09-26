7faf726200075f6943649103ef747fefb2888a69
webheadthwip <dannytran3@gmail.com>
2022-09-25 21:43:13 -0400

    Added unit testing with jest.

--- meadowlark.js
+++ meadowlark.js
@@ -1,45 +1,50 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
+const fortune = require('./lib/fortune')
+const handlers = require('./lib/handlers')
+
 const app = express()
 const port = process.env.PORT || 3000
 
 // configure Handlebars view engine
 const hbs = expressHandlebars.create({ defaultLayout: 'main', })
 
-const fortunes = [
-    "Conquer your fears or they will conquer you.",
-    "Rivers need springs.",
-    "Do not fear what you don't know.",
-    "You will have a pleasant surprise.",
-    "Whenever possible, keep it simple.",
-]
 
 app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
 
 app.use(express.static(__dirname + '/public'))
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
+// WITHOUT USING lib/handlers.js
+// app.get('/', (req, res) => {
+//     res.render('home')
+// })
 
-app.get('/about', (req, res) => {
-    const randomFortune = fortunes[Math.floor(Math.random()*fortunes.length)]
-    res.render('about', { fortune: randomFortune })
-})
+// app.get('/about', (req, res) => {
+//     res.render('about', { fortune: fortune.getFortune() } )
+// })
+
+// // custom 404 page
+// app.use((req, res) => {
+//     res.status(404)
+//     res.render('404')
+// })
+
+// // custom 500 page
+// app.use((err, req, res, next) => {
+//     console.error(err.message)
+//     res.status(500)
+//     res.render('500')
+// })
+
+app.get('/', handlers.home)
+app.get('/about', handlers.about)
 
 // custom 404 page
-app.use((req, res) => {
-    res.status(404)
-    res.render('404')
-})
+app.use(handlers.notFound)
 
 // custom 500 page
-app.use((err, req, res, next) => {
-    console.error(err.message)
-    res.status(500)
-    res.render('500')
-})
+app.use(handlers.serverError)
 
 app.listen(port, () => console.log(
     `Express started on http://localhost:${port}; ` +
